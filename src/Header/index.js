a0439f541722e3a63b54a9969ad5585f553df842
leticia <leticiatrindade0504@gmail.com>
2023-01-06 22:57:59 -0300

    build

--- src/Header/index.js
+++ src/Header/index.js
@@ -4,30 +4,57 @@ import React from "react";
 function Header() {
     return (
 
-        
+        <nav class="nav">
+            <a class="logo col-md-3" href="#">LOGO</a>
 
-        <nav class="nav navbar navbar-expand-lg ">
-            <a class="logo navbar-brand col-md-5" href="#">LOGO</a>
-            <button class="my-toggle navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-                
-            </button>
 
-            <div class="menu collapse navbar-collapse" id="navbarTogglerDemo02">
-                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                    <li class="nav-item active">
-                        <a class=" item  nav-link" href="#">CADASTRAR NOTÍCIAS<span class="sr-only"></span></a>
+            <div class="menu no-responsive col-md-6">
+                <ul class="itens">
+                    <li class="item active">
+                        <a class=" link" href="#">CADASTRAR NOTÍCIAS<span class="sr-only"></span></a>
                     </li>
-                    <li class="nav-item">
-                        <a class=" item  nav-link" href="#">EXIBIR NOTÍCIAS</a>
+                    <li class="item">
+                        <a class=" link " href="#">EXIBIR NOTÍCIAS</a>
                     </li>
-                   
+                    <li>
+                        <form class=" item my-input-group d-flex">
+                            <input class="my-input-text" type="search" placeholder="" />
+                            <button class="my-btn " type="submit"></button>
+                        </form>
+
+                    </li>
+
                 </ul>
 
-                <form class=" my-input-group d-flex">
-                    <input class="my-input-text" type="search" placeholder=""/>
-                        <button class="my-btn " type="submit"></button>
-                </form>
+
             </div>
+
+
+            <div class="menu responsive col-md-6">
+
+                <ul class="itens">
+                    <li className="item"><button className="my-toggle"></button>
+                        <ul>
+                            <li class=" active">
+                                <a class=" link" href="#">CADASTRAR NOTÍCIAS<span class="sr-only"></span></a>
+                            </li>
+                            <li>
+                                <a class=" link  " href="#">EXIBIR NOTÍCIAS</a>
+                            </li>
+                            <li><div className="divli">
+                                <form class=" my-input-group d-flex">
+                                    <input class="my-input-text" type="search" placeholder="" />
+                                    <button class="my-btn " type="submit"></button>
+                                </form>
+                            </div>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+
+
+            </div>
+
         </nav>
 
 
